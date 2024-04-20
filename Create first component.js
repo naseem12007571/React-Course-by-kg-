fb086f9ad4094a7221cbaf22209a23da08483a75
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-04-20 23:47:35 +0530

    Create Create first component.js

---
+++ Create first component.js
@@ -0,0 +1,22 @@
+import Kgbutton from "./kgbutton";
+import Hello from "./Hello";
+import Random from "./Random";
+
+
+function App() {
+  return (
+    <div>
+      <h1>hello how are you</h1>
+      <Kgbutton></Kgbutton>
+      <Hello></Hello>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+      <Random></Random>
+     
+    </div>
+  );
+}
+
+export default App;
