50060ee5a8864c058e475dd306f1cd859d687037
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-04-24 23:19:52 +0530

    Create component.jsx

---
+++ component.jsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+import Styles from "./Container.module.css";
+
+const Container = (props) => {
+  return <div className={Styles.container}>{props.children}</div>;
+};
+
+export default Container;
