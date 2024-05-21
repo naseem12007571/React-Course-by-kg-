b81057bd160cd01d2035c7c1b0916edfb4389694
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-05-21 23:42:04 +0530

    Create cahi app.jsx

---
+++ cahi app.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./chai"
+function App() {
+
+
+  return (
+    <div>
+           <Chai/>
+           <p>hi meri jaan </p>
+           <h2> settinf how are </h2>
+    </div>
+   
+  )
+}
+
+export default App
