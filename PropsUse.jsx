7bbfb06e9edd31eeec1a640582c1cf68a63dbb73
Naseem Ahmad <naseemchoudhary18@gmail.com>
2024-06-10 23:53:11 +0530

    Create PropsUse.jsx

---
+++ PropsUse.jsx
@@ -0,0 +1,8 @@
+
+function PropsUse({info,Email, children}){   // inside chai we will pass props
+    return(
+        <h3> my name is naseem and my mail is =  {info.name} {Email}   {children}</h3>
+    )
+}
+
+export default PropsUse
